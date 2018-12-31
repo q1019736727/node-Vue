55e432aa70fe5ee62aa00b6aae876eee928c0718
ChiuYung <1837060380@qq.com>
2018-12-31 22:57:15 +0800

    no message

--- node-vue/config/index.js
+++ node-vue/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/monitor/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
